2b1e85a27485d424c331f6ac98558782d1224430
P Omkareshwar Reddy <coolomkar11@gmail.com>
2026-07-26 21:14:42 +0530

    Configure adaptive interview length

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 window.APP_CONFIG = {
-  // Paste the deployed Google Apps Script Web App URL here after deployment.
   API_URL: "https://script.google.com/macros/s/AKfycbydSxC4APe9WYBMmznxJC1W9pSKATBJAVMvP3HkmN12mivxAIamLdO-zRbuUcmInEM/exec",
-  QUESTION_COUNT: 8,
+  MIN_QUESTIONS: 8,
+  TARGET_QUESTIONS: 12,
+  MAX_QUESTIONS: 16,
   AUDIO_CHUNK_MS: 90000
 };
