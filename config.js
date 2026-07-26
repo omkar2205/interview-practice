fa50ae94bdb8454213ad8ac8b8726ecaa97ab4ad
P Omkareshwar Reddy <coolomkar11@gmail.com>
2026-07-26 21:05:40 +0530

    Set API_URL to the deployed Google Apps Script URL

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = {
   // Paste the deployed Google Apps Script Web App URL here after deployment.
-  API_URL: "",
+  API_URL: "https://script.google.com/macros/s/AKfycbydSxC4APe9WYBMmznxJC1W9pSKATBJAVMvP3HkmN12mivxAIamLdO-zRbuUcmInEM/exec",
   QUESTION_COUNT: 8,
   AUDIO_CHUNK_MS: 90000
 };
